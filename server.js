6839437e44bc559bd887c81b7045e3ff8609c9a5
Ninh Le <levanninh0101@gmail.com>
2018-11-03 12:49:35 +0700

    hello

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app_1 = require("./app");
 const server = app_1.default.listen(app_1.default.get("port"), app_1.default.get("address"), () => {
     console.log("App is running on http://%s:%d in %s mode", app_1.default.get("address"), app_1.default.get("port"), app_1.default.get("env"));
 });
-
+// const server = app.listen(app.get("port"), () => {
+//     console.log(
+//         "App is running on http://localhost:%d in %s mode",
+//         app.get("port"),
+//         app.get("env")
+//     )
+// });
 exports.default = server;
 //# sourceMappingURL=server.js.map
